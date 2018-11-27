bf5f3b19714ab6a95e836f540e1b07f87aa6884d
rsol7 <rsolm72@gmail.com>
2018-11-27 14:00:18 -0700

    Push a hash or object Kata

---
+++ 8-kyu/Push a hash or an object into array/index.js
@@ -0,0 +1,19 @@
+/*
+
+    You are trying to put a hash in ruby or an object in javascript or java into an array,
+    but it always returns error, solve it and keep it as simple as possible!
+
+    Kata Link:
+        https://www.codewars.com/kata/527b3cd0492b6b15250060af
+
+    Discuss Link:
+        https://www.codewars.com/kata/push-a-hash-slash-an-object-into-array/discuss/javascript
+
+    Solutions Link:
+        https://www.codewars.com/kata/push-a-hash-slash-an-object-into-array/solutions/javascript
+*/
+
+items = []
+items.push({a: "b", c: "d"});
+
+module.exports = items;
---
+++ 8-kyu/Push a hash or an object into array/index.test.js
@@ -0,0 +1,7 @@
+const items = require('.');
+
+describe("solution", () => {
+    it("test for something", () => {
+        expect( typeof items[0]).toBe('object');
+    });
+});
