bf1bba40bab9a349fd91a1bfb6957a828ab1b713
rsol7 <rsolm72@gmail.com>
2018-11-30 16:07:12 -0700

    simple hello world  Ruby Kata

---
+++ 8-kyu/Function 1 - hello world/index.rb
@@ -0,0 +1,18 @@
+
+# Description:
+#     Write a function that merges two sorted arrays into a single one. The arrays only contain integers. 
+#     Also, the final outcome must be sorted and not have any duplicate.
+
+# Kata Link:
+#     https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+# Discuss Link:
+#     https://www.codewars.com/kata/function-1-hello-world/discuss/ruby
+
+# Solutions Link:
+#     https://www.codewars.com/kata/function-1-hello-world/solutions/ruby
+
+
+def greet
+    return "hello world!"
+end
